b0915b149e8b16cb19f27824c2f9c7db0bf539ec
Yatin Gupta <yatin.gupta@OPTIMUSDOM.COM>
2018-09-25 20:05:19 +0530

    Validation Added to Movie Form

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Input from "./common/Input";
 import Select from "./common/Select";
 import _ from "lodash";
+import Form from "./common/Form";
+import Joi from "joi-browser";
 
 class MovieForm extends Component {
   state = {
@@ -10,7 +12,28 @@ class MovieForm extends Component {
       genre: "Thriller",
       noInStock: 0,
       rate: ""
-    }
+    },
+    errors: {}
+  };
+
+  schema = {
+    title: Joi.string()
+      .min(5)
+      .max(30)
+      .required()
+      .label("Title"),
+    genre: Joi.string()
+      .valid("Action", "Comedy", "Thriller")
+      .required()
+      .label("Genre"),
+    noInStock: Joi.number()
+      .integer()
+      .min(0)
+      .required()
+      .label("Number in Stock"),
+    rate: Joi.number()
+      .required()
+      .label("Rate")
   };
 
   getMovieByName = (name, movies) => {
@@ -18,8 +41,54 @@ class MovieForm extends Component {
     return movies[index];
   };
 
-  changeHandler() {
-    console.log("change");
+  changeHandler = e => {
+    let { account } = this.state;
+    let { name: targetName, value: targetValue } = e.currentTarget;
+    account[targetName] = targetValue;
+    let errors = {};
+    let schema = {
+      [targetName]: this.schema[targetName]
+    };
+    let formFieldError = Form.validateField(
+      { [targetName]: targetValue },
+      schema
+    );
+    errors[formFieldError.key] = formFieldError.value;
+    this.setState({ account, errors }); // stands for ({account:account})
+  };
+
+  submitHandler = e => {
+    e.preventDefault();
+    //console.log(this.username.current.value); //this.username.current got reference for HTML Input element
+    const formErrors = Form.validateAllFields(this.state.account, this.schema);
+    let errors = {};
+    formErrors.forEach(function(error) {
+      errors[error.key] = error.value;
+    });
+    this.setState({ errors });
+    // call to server made
+  };
+
+  getErrorByField = field => {
+    if (_.isEmpty(this.state.errors) || this.state.errors[field] === undefined)
+      return "";
+    return this.state.errors[field];
+  };
+
+  getButton(label) {
+    if (Form.validateAllFields(this.state.account, this.schema).length > 0) {
+      return (
+        <button type="submit" className="btn btn-default" disabled>
+          {label}
+        </button>
+      );
+    } else {
+      return (
+        <button type="submit" className="btn btn-default">
+          {label}
+        </button>
+      );
+    }
   }
 
   render() {
@@ -38,13 +107,16 @@ class MovieForm extends Component {
             name={"title"}
             type={"text"}
             onChange={this.changeHandler}
-            errors={"this.getErrorByField('title')"}
+            errors={this.getErrorByField("title")}
           />
           <Select
             genres={["Action", "Comedy", "Thriller"]}
             name="genre"
             id="genre"
-            selectedGenre={this.state.account.genre}
+            defaultValue={this.state.account.genre}
+            errors={this.getErrorByField("genre")}
+            label="Select Genre:"
+            onChange={this.changeHandler}
           />
           <Input
             label={"Number in Stock:"}
@@ -54,7 +126,7 @@ class MovieForm extends Component {
             name={"noInStock"}
             type={"text"}
             onChange={this.changeHandler}
-            errors={"this.getErrorByField('noInStock')"}
+            errors={this.getErrorByField("noInStock")}
           />
           <Input
             label={"Rate:"}
@@ -64,8 +136,13 @@ class MovieForm extends Component {
             name={"rate"}
             type={"text"}
             onChange={this.changeHandler}
-            errors={"this.getErrorByField('rate')"}
+            errors={this.getErrorByField("rate")}
           />
+          <div className="form-group">
+            <div className="col-sm-offset-2 col-sm-10">
+              {this.getButton("Add Movie")}
+            </div>
+          </div>
         </form>
         {/* <h1>Movie Form</h1>
         <div>Movie Id: {movie._id}</div>
--- src/components/common/Select.jsx
+++ src/components/common/Select.jsx
@@ -2,19 +2,25 @@ import React, { Component } from "react";
 
 class Select extends Component {
   state = {};
+  renderErrors = errors => {
+    if (errors === "") return;
+    return <div className="text-danger">{errors}</div>;
+  };
   render() {
+    const { label, id, onChange, defaultValue, name, errors } = this.props;
     return (
       <React.Fragment>
         <div className="form-group">
-          <label htmlFor={this.props.id} className="control-label col-sm-2">
-            Select list:
+          <label htmlFor={id} className="control-label col-sm-2">
+            {label}
           </label>
           <div className="col-sm-10">
             <select
               className="form-control"
               id={this.props.id}
-              name={this.props.name}
-              defaultValue={this.props.selectedGenre}
+              name={name}
+              defaultValue={defaultValue}
+              onChange={onChange}
             >
               {this.props.genres.map(genre => (
                 <option key={genre} value={genre}>
@@ -22,6 +28,7 @@ class Select extends Component {
                 </option>
               ))}
             </select>
+            {this.renderErrors(errors)}
           </div>
         </div>
       </React.Fragment>
